b75ccef465653f47e1cd189c5d9f75732c4ddd44
gregodprogrammer <gregodprogrammer@gmail.com>
2025-09-01 21:29:53 +0100

    update app.js for lastest modifications

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello DevOps World!');
+    res.send('Hello DevOps World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`App running on http://127.0.0.1:${PORT}`);
 });
